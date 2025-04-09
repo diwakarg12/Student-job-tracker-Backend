abb1cd0be17afe68c8fe5dcf90445fee6b4d920e
Diwakar Giri <diwakargiri23@gmail.com>
2025-04-09 18:47:31 +0530

    added apiValidation module to valdiate the req data, cloudinary setup for profile photo processing and userAuth middleware to authenticate if the user is loggedIn or not

---
+++ src/middlewares/userAuth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+const userAuth = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        console.log('Token', token);
+
+        if (!token) {
+            return res.status(401).json({ message: "No token found, Please Login Again" });
+        }
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = decodedData._id;
+
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).json({ message: "No user Found!" });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(500).json({ message: "Error", error: error.message });
+    }
+}
+
+module.exports = userAuth;
---
+++ src/utils/apiValidation.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const validator = require('validator');
+
+const signupValidation = (data) => {
+    const { firstName, lastName, email, password, age, gender } = data;
+    const allowedGender = ['male', 'female', 'other'];
+    if (!validator.isLength(firstName, { min: 3, max: 20 })) {
+        throw new Error("FirstName should be between 3 to 20 Characters");
+    } else if (!validator.isLength(lastName, { min: 3, max: 20 })) {
+        throw new Error("lastName should be between 3 to 20 Characters");
+    } else if (!validator.isEmail(email)) {
+        throw new Error("Email is not Valid");
+    } else if (!validator.isStrongPassword(password)) {
+        throw new Error("password is not Valid");
+    } else if (!validator.isInt(data.age.toString(), { min: 18 })) {
+        throw new Error("Use should be more than 18 yrs old");
+    } else if (!allowedGender.includes(gender)) {
+        throw new Error("Gender is not Valid");
+    }
+};
+
+const loginValidation = (data) => {
+    const { email, password } = data;
+
+    if (!validator.isEmail(email)) {
+        throw new Error("Email is not Valid");
+
+    } else if (!validator.isStrongPassword(password)) {
+        throw new Error("Password is not Valid");
+    }
+};
+
+function isValidImageUrlOrBase64(str) {
+    const isImageUrl = validator.isURL(str, { require_protocol: true }) && /\.(jpg|jpeg|png|gif|bmp|webp|svg)$/i.test(str);
+    const isBase64 = /^data:image\/(png|jpeg|jpg|gif|bmp|webp);base64,/i.test(str);
+    return isImageUrl || isBase64;
+};
+
+const updateValidation = (data) => {
+    const updatable = ["firstName", "lastName", "gender", "age", "about", "skills", "profileUrl"];
+
+    const isUpdatable = Object.keys(data).every(req => updatable.includes(req))
+    if (!isUpdatable) {
+        throw new Error("Invalid Update Request");
+    } else if (data.firstName && !validator.isLength(data.firstName, { min: 3, max: 20 })) {
+        throw new Error("firstName should be between 3 to 20 Characters");
+    } else if (data.lastName && !validator.isLength(data.lastName, { min: 3, max: 20 })) {
+        throw new Error("firstName should be between 3 to 20 Characters");
+    } else if (data.age && !validator.isInt(data.age.toString(), { min: 18 })) {
+        throw new Error("Age should be more than 18 Years");
+    } else if (data.about && !validator.isLength(data.about, { min: 20, max: 400 })) {
+        throw new Error("About should be between 20 to 400 Character");
+    } else if (data.skills && data.skills.length > 10) {
+        throw new Error("Skills Should be less than 10");
+    } else if (data.profileUrl && !isValidImageUrlOrBase64(data.profileUrl)) {
+        throw new Error("Invalid profile URL");
+    }
+};
+
+const updatePasswordValidation = (password) => {
+    if (!validator.isStrongPassword(password)) {
+        throw new Error("Password is not Valid");
+    }
+};
+
+const applyJob = (data) => {
+    const { company, role, status } = data;
+
+    if (!validator.isLength(company, { min: 3, max: 20 })) {
+        throw new Error('company Name should be between 3 to 20 Characters')
+    } else if (!validator.isLength(role, { min: 3, max: 20 })) {
+        throw new Error('Role should be between 3 to 20 Characters');
+    } else if (!validator.isLength(status, { min: 3, max: 20 })) {
+        throw new Error('Status should be between 3 to 20 Characters');
+    }
+};
+
+module.exports = {
+    applyJob,
+    signupValidation,
+    loginValidation,
+    updateValidation,
+    updatePasswordValidation
+};
